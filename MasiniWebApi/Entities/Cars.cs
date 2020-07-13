fc7963d6042c75f8551927b4936e9d1cefeab5ad
gal teodora <gal_teodora@yahoo.com>
2020-07-14 00:29:01 +0300

    adding solution

---
+++ MasiniWebApi/Entities/Cars.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasiniWebApi.Entities
+{
+    public class Cars
+    {
+        [Key]
+        public Guid ID { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; }
+
+        [MaxLength(2500)]
+        public string Descriptiom { get; set; }
+
+        [Required]
+        public string ModelId { get; set; }
+
+        [ForeignKey("ModelId")]
+        public virtual Model Model { get; set; }
+
+        public bool? Deleted { get; set; }
+    }
+}
---
+++ MasiniWebApi/Entities/Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasiniWebApi.Entities
+{
+    public class Model
+    {
+        [Key]
+        public Guid ID { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; }
+
+
+        public bool? Deleted { get; set; }
+    }
+}
